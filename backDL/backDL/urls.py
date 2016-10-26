10573ae917a09fbf9e1f7ebd9b8f32a42c46a7f2
Gonzalo Flores <gonzalo@rayalab.cl>
2016-10-26 08:03:32 -0300

    add vueloferta

--- backDL/backDL/urls.py
+++ backDL/backDL/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^emails/', include('emails.urls')),
+    url(r'^vueloferta/', include('vueloferta.urls')),
 ]
